91742a167906776be2e3a9f7252da93cf645c448
Rüçhan Okal <ruchanokal@gmail.com>
2022-07-23 15:25:05 +0300

    1.2 -> Night mode disabled

--- app/src/main/java/com/ruchanokal/carinstructions/activities/MainActivity.kt
+++ app/src/main/java/com/ruchanokal/carinstructions/activities/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -15,30 +17,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private val TAG = "MainActivity"
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         val prefences = getSharedPreferences("com.ruchanokal.carinstructions", Context.MODE_PRIVATE)
-        auth = Firebase.auth
-
-        auth.signInAnonymously()
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInAnonymously:success")
-
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInAnonymously:failure", task.exception)
-
-                }
-            }
-
 
         val currentAppLocale = Locale.getDefault().getLanguage()
         Log.i(TAG,"currentAppLocale: " + currentAppLocale)
